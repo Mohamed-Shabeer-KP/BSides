70488e1ebf7b3906cff33bbb7c0478d666f12512
N3CR0 V3N0M <mohammedshabeerkp@gmail.com>
2020-02-10 23:08:54 +0530

    minor fix

--- web/index.php
+++ web/index.php
@@ -79,7 +79,7 @@
   <header class="masthead">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
-      <img class="w-50" src="img/bsides-logo.png"  alt="">
+      <img class="w-75" src="img/bsides-logo.png"  alt="">
         <h2 class="text-white-50 mx-auto mt-2 mb-5">
         
         </h2>
