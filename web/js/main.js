5887c287b8d559c178717fd3dedeaa030be11092
Mohamed-Shabeer-KP <mohammedshabeerkp@gmail.com>
2021-04-21 11:51:12 +0530

    Made Post changes after event

---
+++ web/js/main.js
@@ -0,0 +1,39 @@
+
+$(function () {
+  /* Preloader*/
+  $("#loading").load("/page/preloader.html"); 
+
+$(window).on("load", function(){
+  $('#loading').fadeOut('slow', function() {
+    $(this).remove();
+  });
+});
+
+  var delay = 6 * 1000; 
+
+  /* COUNTDOWN  */
+  function getCountDown() {
+      return new Date(new Date().valueOf() + (delay-1000));
+  }
+
+  setTimeout(function() {
+    $('#tload').html('');
+    $('#clock-c').html('<span class="h1 font-weight-bold">EVENT FINISHED</span>');
+   }, delay);
+
+
+  $('#clock-c').countdown(getCountDown(), function(event) {
+    var $this = $(this).html(event.strftime(''
+      + '<span class="h1 font-weight-bold">%D</span> Day%!d'
+      + '<span class="h1 font-weight-bold">%H</span> Hr'
+      + '<span class="h1 font-weight-bold">%M</span> Min'
+      + '<span class="h1 font-weight-bold">%S</span> Sec'));
+  });
+
+
+  
+});
+
+$(document).ready(function(){
+  $('.aniview').AniView();
+});
